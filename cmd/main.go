25ea18cd962e95dca5b410144971e0fe82e35093
amicie-monami <cvetochek0@list.ru>
2025-10-18 21:09:30 +0300

    add: dummy writers handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
-package cmd
+package main
 
 import (
+	"trmp/internal/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	serv := gin.Default()
+	routes.SetupRoutes(serv)
 	serv.Run()
 }
--- internal/routes/writers.go
+++ internal/routes/writers.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"net/http"
+	"trmp/internal/model"
+
 	"github.com/gin-gonic/gin"
 )
 
 func writersHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// ctx.JSON(http.StatusOK, writer)
+		writer := model.WriterCard{
+			ID:          1,
+			Name:        "Sergey",
+			PortraitURL: "test.url",
+			IsFavorite:  false,
+		}
+
+		ctx.JSON(http.StatusOK, writer)
 	}
 }
